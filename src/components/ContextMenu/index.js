2769aea17048954cc288f12b271505c67e46dcdf
Wolfgang Schuster <wschuster@shoretel.com>
2017-03-29 11:46:51 -0500

    Add shape icons. Imrpove menu colors.

--- src/components/ContextMenu/index.js
+++ src/components/ContextMenu/index.js
@@ -8,18 +8,33 @@ import * as drawingActions from 'actions/drawing';
 import * as themeActions from 'actions/theme';
 import { pieSlicePath } from 'root/shapes';
 
+const radius = 100;
+const iconAngle = (start, end) => Math.PI * (start + (end - start) / 2) / 180;
+const iconX = (start, end) => radius / 2 * Math.cos(iconAngle(start, end));
+const iconY = (start, end) => radius / 2 * Math.sin(iconAngle(start, end));
 const menuSlice = ({
+    child,
     end,
+    key,
     start,
     ...pathProps
 }) => (
-    <path
-        className='pieMenuSlice'
-        d={pieSlicePath(0, 0, start, end, 100)}
-        // fill={'red'}
-        // stroke={styles[colors].grid}
-        {...pathProps}
-    />
+    <g
+        key={key}
+    >
+        <path
+            className='pie-menu--selectable'
+            d={pieSlicePath(0, 0, start, end, radius)}
+            {...pathProps}
+        />
+        <g
+            style={{
+                transform: `translate(${iconX(start, end)}px, ${iconY(start, end)}px)`
+            }}
+        >
+            {child}
+        </g>
+    </g>
 );
 
 @connect(({
@@ -43,6 +58,7 @@ class ContextMenu extends PureComponent {
         super(props);
 
         this.handleClickOutside = this.handleClickOutside.bind(this);
+        this.handleSetShape = this.handleSetShape.bind(this);
     }
 
     render() {
@@ -54,34 +70,47 @@ class ContextMenu extends PureComponent {
         } = this.props;
         const menuOptions = [
             {
-                onClick: () => dispatch(drawingActions.setShapeType('rect')),
+                onClick: () => this.handleSetShape('rect'),
                 child:
                     <rect
                         x='0'
                         y='0'
-                        width='10'
-                        height='10'
+                        width='20'
+                        height='20'
+                        fill='none'
+                        stroke='black'
+                        strokeWidth='3px'
                     />,
             },
             {
-                onClick: () => dispatch(drawingActions.setShapeType('circle')),
+                onClick: () => this.handleSetShape('circle'),
                 child:
                     <circle
                         cx='0'
                         cy='0'
-                        r='5'
+                        r='10'
+                        fill='none'
+                        stroke='black'
+                        strokeWidth='3px'
                     />,
             },
             {
-                onClick: () => dispatch(drawingActions.setShapeType('line')),
+                onClick: () => this.handleSetShape('line'),
                 child:
                     <line
                         x1='0'
-                        y1='10'
-                        x2='10'
+                        y1='20'
+                        x2='20'
                         y2='0'
+                        stroke='black'
+                        strokeWidth='3px'
                     />,
             },
+            // {
+            //     onClick: () => his.handleSetShape('path'),
+            //     // child:
+            //     //     'icon',
+            // },
         ].map((option, i, arr) => menuSlice({
             end: 360 / arr.length * (i + 1),
             key: `context-menu__option__${i}`,
@@ -100,7 +129,7 @@ class ContextMenu extends PureComponent {
                 <circle
                     cx='0'
                     cy='0'
-                    r='100'
+                    r={radius}
                     fill={styles[colors].background}
                     stroke={styles[colors].grid}
                     filter='url(#dropShadow)'
@@ -112,6 +141,13 @@ class ContextMenu extends PureComponent {
                     r='20'
                     fill={styles[colors].background}
                     stroke={styles[colors].grid}
+                />
+                <circle
+                    cx='0'
+                    cy='0'
+                    r='20'
+                    className='pie-menu--selectable'
+                    stroke='none'
                     onClick={() => dispatch(themeActions.toggleColor())}
                 />
             </g>
@@ -120,6 +156,15 @@ class ContextMenu extends PureComponent {
 
     handleClickOutside() {
         this.props.dispatch(contextMenuActions.show(false));
+    }
+
+    handleSetShape(type) {
+        const {
+            dispatch,
+        } = this.props;
+
+        dispatch(drawingActions.setShapeType(type));
+        dispatch(contextMenuActions.show());
     }
 }
 
--- src/shapes.js
+++ src/shapes.js
@@ -4,5 +4,5 @@ export const pieSlicePath = (startX, startY, startAngle, endAngle, radius) => {
     const x2 = startX + radius * Math.cos(Math.PI * endAngle / 180);
     const y2 = startY + radius * Math.sin(Math.PI * endAngle / 180);
 
-    return `M${startX} ${startY} L${startX + radius * Math.cos(Math.PI * startAngle/180)} ${y1} A${radius} ${radius} 0 0 1 ${x2} ${y2} z`;
+    return `M${startX} ${startY} L${startX + radius * Math.cos(Math.PI * startAngle / 180)} ${y1} A${radius} ${radius} 0 0 1 ${x2} ${y2} z`;
 };
